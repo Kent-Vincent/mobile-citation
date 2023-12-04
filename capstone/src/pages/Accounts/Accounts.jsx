11456cb42968732c4c4c43983bb65669a7f7d252
jsvllnv <jaszmeineabigaille.villanueva@hcdc.edu.ph>
2023-12-04 10:58:14 +0800

    accounts

---
+++ capstone/src/pages/Accounts/Accounts.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Box from '@mui/material/Box';
+import Dashboard from '../../Components/Dashboard/Dashboard';
+import Toolbar from '@mui/material/Toolbar';
+import AccountList from './AccountList'
+
+
+
+export default function Accounts() {
+  return (
+    <>
+    <Box sx={{ display: 'flex', marginLeft: '-50px' }}>
+        <Dashboard/>  
+        <Box component="main"
+        sx={{ flexGrow: 1,  p: 1 }}
+      >
+        <Toolbar />
+        <h1 style={{ margin: '15px 0', color:'#00425A' }}>Accounts</h1>
+        <AccountList/>
+      </Box>
+    </Box>
+    </>
+    
+  )
+}
