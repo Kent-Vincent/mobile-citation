b3437f9be464ad65103a5525f706848c9280495f
jsvllnv <jaszmeineabigaille.villanueva@hcdc.edu.ph>
2023-12-04 21:25:23 +0800

    wala nay three bars

--- capstone/src/pages/Navbar.js
+++ capstone/src/pages/Navbar.js
@@ -134,15 +134,6 @@ export default function Navbar() {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="fixed">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="open drawer"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography
             variant="h6"
             noWrap
