961a3e68c3a62ba0cf178d5d57097c4eb716e7cd
Kent-Vincent <sarsalejo02@gmail.com>
2023-12-05 13:32:41 +0800

    need update

--- capstone/src/pages/Accounts/AccountList.js
+++ capstone/src/pages/Accounts/AccountList.js
@@ -16,8 +16,8 @@ import TextField from '@mui/material/TextField';
 import {ref, onValue, getDatabase } from 'firebase/database';
 
 const columns = [
-  { id: 'name', label: 'Name' },
-  { id: 'email', label: 'Email' },
+  { id: 'name', label: 'Name', minWidth: 150 },
+  { id: 'email', label: 'Email', minWidth: 100 },
   ];
 
   function createData(name, email) {
