a0d0bf95c8093e851d0d701ea6a09d9e4f37a693
Kent-Vincent <sarsalejo02@gmail.com>
2023-12-13 20:57:10 +0800

    added Loading to assessment

--- capstone/src/pages/Analysis/AssesmentList.js
+++ capstone/src/pages/Analysis/AssesmentList.js
@@ -14,6 +14,7 @@ import {
 import { ref, onValue, getDatabase } from 'firebase/database';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import Loading from '../Loading';
 
 const ViolationsTable = ({ violations, isExpanded }) => {
   return (
@@ -49,33 +50,34 @@ const AssessmentList = () => {
   const [rows, setRows] = useState([]);
   const [repetitionCount, setRepetitionCount] = useState({});
   const [selectedRow, setSelectedRow] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const database = getDatabase();
 
     const fetchData = async () => {
       const dataRef = ref(database, 'uploads/Information');
-      
+
       try {
         onValue(dataRef, (snapshot) => {
           const data = snapshot.val();
-      
+
           if (data) {
             const uniqueRows = new Set();
             const countMap = {};
-      
+
             Object.values(data).forEach((innerData) => {
               Object.values(innerData).forEach((row) => {
                 const uniqueKey = `${row.Name}-${row.LicenseNumber}-${row.PlateNumber}`;
                 uniqueRows.add(uniqueKey);
-      
+
                 // Count repetitions
                 countMap[uniqueKey] = (countMap[uniqueKey] || 0) + 1;
-      
+
                 row.Violation = row.Violation.replace(/\\\\n/g, '\n');
               });
             });
-      
+
             const newData = Array.from(uniqueRows).map((uniqueKey) => {
               const [Name, LicenseNumber, PlateNumber] = uniqueKey.split('-');
               return {
@@ -92,17 +94,19 @@ const AssessmentList = () => {
                 ),
               };
             });
-      
+
             console.log('Fetched Data: ', newData);
             setRows(newData);
             setRepetitionCount(countMap);
             setSelectedRow(null);
+            setLoading(false);
           }
         });
       } catch (error) {
         console.error('Error setting up data listener:', error);
+        setLoading(false);
       }
-    };    
+    };
 
     fetchData();
 
@@ -116,53 +120,58 @@ const AssessmentList = () => {
   };
 
   return (
-    <TableContainer component={Paper}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell />
-            <TableCell>Name</TableCell>
-            <TableCell>License Number</TableCell>
-            <TableCell>Plate Number</TableCell>
-            <TableCell>Repetition Count</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row, index) => (
-            <React.Fragment key={index}>
+    <>
+      {loading && <Loading />}
+      {!loading && (
+        <TableContainer component={Paper}>
+          <Table>
+            <TableHead>
               <TableRow>
-                <TableCell>
-                  <IconButton
-                    size="small"
-                    onClick={() => handleRowToggle(`${row.Name}-${row.LicenseNumber}-${row.PlateNumber}`)}
-                  >
-                    {selectedRow === `${row.Name}-${row.LicenseNumber}-${row.PlateNumber}` ? (
-                      <KeyboardArrowUpIcon />
-                    ) : (
-                      <KeyboardArrowDownIcon />
-                    )}
-                  </IconButton>
-                </TableCell>
-                <TableCell>{row.Name}</TableCell>
-                <TableCell>{row.LicenseNumber}</TableCell>
-                <TableCell>{row.PlateNumber}</TableCell>
-                <TableCell>{repetitionCount[`${row.Name}-${row.LicenseNumber}-${row.PlateNumber}`]}</TableCell>
+                <TableCell />
+                <TableCell>Name</TableCell>
+                <TableCell>License Number</TableCell>
+                <TableCell>Plate Number</TableCell>
+                <TableCell>Repetition Count</TableCell>
               </TableRow>
-              {selectedRow === `${row.Name}-${row.LicenseNumber}-${row.PlateNumber}` && (
-                <TableRow>
-                  <TableCell colSpan={5}>
-                    <ViolationsTable
-                      violations={row.Violations}
-                      isExpanded={selectedRow === `${row.Name}-${row.LicenseNumber}-${row.PlateNumber}`}
-                    />
-                  </TableCell>
-                </TableRow>
-              )}
-            </React.Fragment>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+            </TableHead>
+            <TableBody>
+              {rows.map((row, index) => (
+                <React.Fragment key={index}>
+                  <TableRow>
+                    <TableCell>
+                      <IconButton
+                        size="small"
+                        onClick={() => handleRowToggle(`${row.Name}-${row.LicenseNumber}-${row.PlateNumber}`)}
+                      >
+                        {selectedRow === `${row.Name}-${row.LicenseNumber}-${row.PlateNumber}` ? (
+                          <KeyboardArrowUpIcon />
+                        ) : (
+                          <KeyboardArrowDownIcon />
+                        )}
+                      </IconButton>
+                    </TableCell>
+                    <TableCell>{row.Name}</TableCell>
+                    <TableCell>{row.LicenseNumber}</TableCell>
+                    <TableCell>{row.PlateNumber}</TableCell>
+                    <TableCell>{repetitionCount[`${row.Name}-${row.LicenseNumber}-${row.PlateNumber}`]}</TableCell>
+                  </TableRow>
+                  {selectedRow === `${row.Name}-${row.LicenseNumber}-${row.PlateNumber}` && (
+                    <TableRow>
+                      <TableCell colSpan={5}>
+                        <ViolationsTable
+                          violations={row.Violations}
+                          isExpanded={selectedRow === `${row.Name}-${row.LicenseNumber}-${row.PlateNumber}`}
+                        />
+                      </TableCell>
+                    </TableRow>
+                  )}
+                </React.Fragment>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      )}
+    </>
   );
 };
 
